0715cfc854397e3983223989d1d937e330d832f4
Nur Alom <md9577630@gmail.com>
2026-01-28 23:34:16 +0600

    make meals section responsive

--- src/components/layouts/meals.tsx
+++ src/components/layouts/meals.tsx
@@ -5,12 +5,12 @@ const Meals = async () => {
     const { data: meals } = await getMeals();
 
     return (
-        <div className="px-5 md:px-10 lg:px-20 py-10 grid gap-8 space-y-12">
+        <div className="py-10 md:py-16 lg:py-20 lg:px-16 space-y-12">
             <div className="text-center">
-                <small className="text-sm font-semibold uppercase">Hot & Spicy</small>
-                <h1 className="font-bold text-3xl uppercase text-[#f1620c]">Experience Our Delicious Food</h1>
+                <small className="text-xs md:text-sm font-semibold uppercase">Hot & Spicy</small>
+                <h1 className="font-bold text-xl md:text-3xl uppercase text-[#f1620c]">Experience Our Delicious Food</h1>
             </div>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {
                     meals.slice(0, 12).map((meal: any) => (
                         <MealCard key={meal.id} meal={meal} />
