a0ad205c0a4b5fb144878d62e26280b0708098a7
Nur Alom <md9577630@gmail.com>
2026-01-28 23:37:19 +0600

    make reviews section responsive

--- src/components/layouts/reviews.tsx
+++ src/components/layouts/reviews.tsx
@@ -5,12 +5,12 @@ const Reviews = async () => {
     const { data: reviews } = await getReviews();
 
     return (
-        <div className="px-5 md:px-10 lg:px-20 py-10 grid gap-8 space-y-12">
+        <div className="py-10 md:py-16 lg:py-20 lg:px-16 space-y-12">
             <div className="text-center">
-                <small className="text-sm font-semibold uppercase">For whom we are blessed.</small>
-                <h1 className="font-bold text-3xl uppercase text-[#f1620c]">What our valued customers say about us.</h1>
+                <small className="text-xs md:text-sm font-semibold uppercase">For whom we are blessed.</small>
+                <h1 className="font-bold text-xl md:text-3xl uppercase text-[#f1620c]">What our valued customers say about us.</h1>
             </div>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {
                     reviews.map((review: any, idx: number) => (
                         <ReviewCard key={idx} review={review} />
