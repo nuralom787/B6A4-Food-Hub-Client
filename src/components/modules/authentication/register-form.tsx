3ca50c3b87ed40a7877b4be2efa56849cb59e878
Nur Alom <md9577630@gmail.com>
2026-01-30 15:06:51 +0600

    update registration form

--- src/components/modules/authentication/register-form.tsx
+++ src/components/modules/authentication/register-form.tsx
@@ -22,6 +22,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { authClient } from "@/lib/auth-client";
 import { useForm } from "@tanstack/react-form";
 import { redirect } from "next/navigation";
+import { useState } from "react";
 import { toast } from "sonner";
 import * as z from "zod";
 
@@ -30,9 +31,15 @@ const formSchema = z.object({
   email: z.email(),
   password: z.string().min(8, "Minimum length is 8"),
   role: z.string().min(1, "Role is required"),
+  businessName: z.string(),
+  address: z.string(),
+  description: z.string(),
+  imageUrl: z.url(),
 });
 
 export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const [userType, setUserType] = useState("CUSTOMER");
+
   const handleGoogleLogin = async () => {
     const data = authClient.signIn.social({
       provider: "google",
@@ -48,162 +55,332 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
       email: "",
       password: "",
       role: "CUSTOMER",
+      businessName: "",
+      address: "",
+      description: "",
+      imageUrl: ""
     },
     validators: {
       onSubmit: formSchema,
     },
     onSubmit: async ({ value }) => {
-      // console.log(value)
+      console.log(value)
 
-      const toastId = toast.loading("Creating user...");
-      try {
-        const { data, error } = await authClient.signUp.email(value);
+      // const toastId = toast.loading("Creating user...");
+      // try {
+      //   const { data, error } = await authClient.signUp.email(value);
 
-        if (error) {
-          toast.error(error.message, { id: toastId });
-          return;
-        }
+      //   if (error) {
+      //     toast.error(error.message, { id: toastId });
+      //     return;
+      //   }
 
-        toast.success("User Created Successfully", { id: toastId });
+      //   toast.success("User Created Successfully", { id: toastId });
 
-        redirect("/");
-      } catch (err) {
-        toast.error("Something went wrong, please try again.", { id: toastId });
-      }
+      //   redirect("/");
+      // } catch (err) {
+      //   toast.error("Something went wrong, please try again.", { id: toastId });
+      // }
     },
   });
 
   return (
     <Card {...props}>
-      <CardHeader>
+      <CardHeader className="text-center">
         <CardTitle>Create an account</CardTitle>
         <CardDescription>
           Enter your information below to create your account
         </CardDescription>
+        <form.Field
+          name="role"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid;
+            return (
+              <Field>
+                <FieldLabel htmlFor={field.name}>Select Your Account Type</FieldLabel>
+                <FieldContent>
+                  <RadioGroup
+                    value={field.state.value}
+                    onValueChange={(value) => {
+                      field.handleChange(value);
+                      setUserType(value);
+                    }}
+                    className="flex gap-4"
+                  >
+                    <Label htmlFor="customer" className="flex items-center space-x-2 border border-gray-600 rounded p-2 w-full cursor-pointer">
+                      <RadioGroupItem defaultChecked value="CUSTOMER" id="customer" />
+                      <span >Customer</span>
+                    </Label>
+
+                    <Label htmlFor="provider" className="flex items-center space-x-2 border border-gray-600 rounded p-2 w-full cursor-pointer">
+                      <RadioGroupItem value="PROVIDER" id="provider" />
+                      <span >Provider</span>
+                    </Label>
+                  </RadioGroup>
+                </FieldContent>
+
+                {isInvalid && (
+                  <FieldError errors={field.state.meta.errors} />
+                )}
+              </Field>
+            );
+          }}
+        />
       </CardHeader>
+      <hr className="my-4" />
       <CardContent>
         <form
-          id="login-form"
+          id="register-form"
           onSubmit={(e) => {
             e.preventDefault();
             form.handleSubmit();
           }}
         >
-          <FieldGroup>
-            <form.Field
-              name="name"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched && !field.state.meta.isValid;
-                return (
-                  <Field data-invalid={isInvalid}>
-                    <FieldLabel htmlFor={field.name}>Name</FieldLabel>
-                    <Input
-                      type="text"
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                    {isInvalid && (
-                      <FieldError errors={field.state.meta.errors} />
-                    )}
-                  </Field>
-                );
-              }}
-            />
-            <form.Field
-              name="email"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched && !field.state.meta.isValid;
-                return (
-                  <Field>
-                    <FieldLabel htmlFor={field.name}>Email</FieldLabel>
-                    <Input
-                      type="email"
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                    {isInvalid && (
-                      <FieldError errors={field.state.meta.errors} />
-                    )}
-                  </Field>
-                );
-              }}
-            />
-            <form.Field
-              name="password"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched && !field.state.meta.isValid;
-                return (
-                  <Field>
-                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
-                    <Input
-                      type="password"
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                    {isInvalid && (
-                      <FieldError errors={field.state.meta.errors} />
-                    )}
-                  </Field>
-                );
-              }}
-            />
-            <form.Field
-              name="role"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched && !field.state.meta.isValid;
-                return (
-                  <Field>
-                    <FieldLabel htmlFor={field.name}>User Role</FieldLabel>
-                    <FieldContent>
-                      <RadioGroup
+          {userType === "CUSTOMER" ?
+            <FieldGroup>
+              <form.Field
+                name="name"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                      <Input
+                        type="text"
+                        id={field.name}
+                        name={field.name}
                         value={field.state.value}
-                        onValueChange={field.handleChange}
-                        className="flex gap-6"
-                      >
-                        <div className="flex items-center space-x-2">
-                          <RadioGroupItem defaultChecked value="CUSTOMER" id="customer" />
-                          <Label htmlFor="customer">Customer</Label>
-                        </div>
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+              <form.Field
+                name="email"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field>
+                      <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                      <Input
+                        type="email"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+              <form.Field
+                name="password"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field>
+                      <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                      <Input
+                        type="password"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
 
-                        <div className="flex items-center space-x-2">
-                          <RadioGroupItem value="PROVIDER" id="provider" />
-                          <Label htmlFor="provider">Provider</Label>
-                        </div>
-                      </RadioGroup>
-                    </FieldContent>
-
-                    {isInvalid && (
-                      <FieldError errors={field.state.meta.errors} />
-                    )}
-                  </Field>
-                );
-              }}
-            />
-          </FieldGroup>
+            </FieldGroup>
+            :
+            <FieldGroup>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <form.Field
+                  name="name"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                        <Input
+                          type="text"
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    );
+                  }}
+                />
+                <form.Field
+                  name="email"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field>
+                        <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                        <Input
+                          type="email"
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    );
+                  }}
+                />
+                <form.Field
+                  name="password"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field>
+                        <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                        <Input
+                          type="password"
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    );
+                  }}
+                />
+                <form.Field
+                  name="businessName"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Business Name</FieldLabel>
+                        <Input
+                          type="text"
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    );
+                  }}
+                />
+                <form.Field
+                  name="address"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Business Address</FieldLabel>
+                        <Input
+                          type="text"
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    );
+                  }}
+                />
+                <form.Field
+                  name="description"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Description</FieldLabel>
+                        <Input
+                          type="text"
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    );
+                  }}
+                />
+              </div>
+              <form.Field
+                name="imageUrl"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Business Image URL</FieldLabel>
+                      <Input
+                        type="text"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+            </FieldGroup>
+          }
         </form>
       </CardContent>
       <CardFooter className="flex flex-col gap-5 justify-end">
-        <Button form="login-form" type="submit" className="w-full cursor-pointer">
+        <Button form="register-form" type="submit" className="w-full cursor-pointer">
           Register
         </Button>
-        <Button
+        {/* <Button
           onClick={() => handleGoogleLogin()}
           variant="outline"
           type="button"
           className="w-full cursor-pointer"
         >
           Continue with Google
-        </Button>
+        </Button> */}
       </CardFooter>
     </Card>
   );
